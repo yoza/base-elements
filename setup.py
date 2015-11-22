ca2be37b3147bbe5543cfd120ab6e9d4670c7145
Oleg Prans <oleg@prans.net>
2015-11-22 12:45:43 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+
+setup(name='elements',
+      version='1.0',
+      description='Django base templatetags',
+      author='Oleg Prans',
+      author_email='oleg@prans.net',
+      url='https://github.com/yoza/base-elements.git',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'elements': ['templates/admin/*.html',
+                                 'templates/admin/edit_inline/*.html',
+                                 'templates/admin/edit_inline/hvad/*.html',
+                                 'templates/admin/elements/*.html',
+                                 'templates/blocks/*.html',
+                                 'templates/elements/*.html',
+                                 'static/elements/*/*.*',
+                                 'static/elements/*/src/*.css',
+                                 'locale/*/*/*.*']},
+      install_requires=['django-hvad'],
+      zip_safe=False)
